80bd8425e34b08116e36d840a798dc2f931b5704
ABOUYAAQOUB Hamza <hamzaabouyaaqoub@gmail.com>
2023-05-14 13:13:55 +0100

    Creation de l'API Marque

---
+++ src/main/java/com/ElecVehicule/demo/Repository/MarqueRepository.java
@@ -0,0 +1,9 @@
+package com.ElecVehicule.demo.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ElecVehicule.demo.model.Marque;
+
+public interface MarqueRepository extends JpaRepository<Marque, Long>{
+
+}
